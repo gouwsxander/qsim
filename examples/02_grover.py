57c8511bc20183acf965561f7543739aca8ed0d6
Xander <gouws.xander@gmail.com>
2025-05-12 23:18:11 -0400

    Implements example of Grover's algorithm

---
+++ examples/02_grover.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+import qsim.system
+import qsim.gates
+
+N_BITS = 4
+
+# Create an oracle with a random key which we want to find
+oracle = qsim.gates.get_oracle_gate(N_BITS, key=3)
+
+# Create a n-bit quantum system
+system = qsim.system.System(N_BITS)
+
+# Apply the Hadamard gate to all gates
+hadamard_all = qsim.gates.make_gate(
+    {i: qsim.gates.H_1 for i in range(2**N_BITS)},
+    N_BITS
+)
+system.apply_gate(hadamard_all)
+
+diffusion = -qsim.gates.get_oracle_gate(N_BITS, state=system.state)
+
+# Apply operations
+for i in range(int(np.pi / 4 * np.sqrt(2**N_BITS))):
+    system.apply_gate(oracle)
+    system.apply_gate(diffusion)
+    print(system._get_probabilities())
+
